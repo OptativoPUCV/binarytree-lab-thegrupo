8aa7a0adfb4b342ac72b44314a727c28a695f888
repl.it user <replituser@example.com>
2020-09-30 19:56:57 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -36,9 +36,11 @@ TreeNode * createTreeNode(void* key, void * value) {
 }
 
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
-
-    //new->lower_than = lower_than;
-    return NULL;
+    TreeMap* nuevo=(TreeMap*)malloc(sizeof(TreeMap));
+    nuevo->root=NULL;
+    nuevo->current=NULL; 
+    nuevo->lower_than=lower_than;
+    return nuevo;
 }
 
 
